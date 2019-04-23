e1e52e02a78ec8acc87f9add3d611ced4099839b
Emmanuel Wada <emmanuelwada@Emmanuels-MacBook-Pro.local>
2019-04-23 17:23:46 -0500

    Updates

---
+++ precache-manifest.0867425897dced4fd39559f01ec9903f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae73efe935c286a8adc8",
+    "url": "/Clicky_Game/static/js/runtime~main.1f82a145.js"
+  },
+  {
+    "revision": "12f2290bfeecaac019c1",
+    "url": "/Clicky_Game/static/js/main.d60b9eb7.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/Clicky_Game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "12f2290bfeecaac019c1",
+    "url": "/Clicky_Game/static/css/main.40f31a0e.chunk.css"
+  },
+  {
+    "revision": "37816cc3a717c9854ccb48573c296543",
+    "url": "/Clicky_Game/index.html"
+  }
+];
