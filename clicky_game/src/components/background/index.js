5df8ac591ac62f11b99fbbf20bc02a5941fde2af
Emmanuel Wada <emmanuelwada@Emmanuels-MBP.attlocal.net>
2019-07-12 17:44:56 -0500

    reconfig

--- clicky_game/src/components/background/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./style.css";
-
-function Background(props) {
-  return <div className="Background">{props.children}</div>;
-}
-
-export default Background;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+         Dragon Ball Z Clicky Game! <img alt="dbz clicky" src="assets/images/dbz icon.png" />
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
